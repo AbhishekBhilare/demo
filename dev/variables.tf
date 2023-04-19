f1572a0d9296d612c08582029f64de3969b8b92e
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-04-19 14:45:54 +0530

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -13,15 +13,7 @@ variable "Dev-instance-ami" {
   type        = string
 }
 
-variable "access_key" {
-  type = string
-  default = "AKIAQFGYD5FO3QQSKUIZ"
-}
 
-variable "secret_key" {
-  type = string
-  default = "fduE1HqOexGU776dZv4X76abgXmAQcGH1EPob7hY"
-}
 
 
 
