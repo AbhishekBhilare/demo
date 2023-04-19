8fe439eb937ad063a44415b328c3deff3d758c0e
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-04-19 20:30:49 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -3,5 +3,10 @@ module "ec2" {
   instance-name = var.Dev-instance-name
   instance_ami  = var.Dev-instance-ami
   instance_type = var.Dev-instance-type
+  security_groups_name = "ABHISHEK"
+  from_port = var.from_port
+  to_port = var.to_port
+  key-name = var.key_name
+  no-of-instance = var.no-of-instances
+  
 }
-
