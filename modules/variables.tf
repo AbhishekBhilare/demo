39ff090b3e847bbd9dc3cb618fc57d40ea4d5fa5
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-04-19 20:29:31 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -12,3 +12,25 @@ variable "instance_type" {
 variable "instance_ami" {
     description = "instance ami by defualt it choose free tier ami"
 }
+
+
+
+variable "security_groups_name" {
+  type = string
+}
+
+variable "from_port" {
+  type = list
+}
+
+variable "to_port" {
+  type= list
+}
+
+variable "key-name" {
+  type = string
+}
+
+variable "no-of-instance" {
+  type = number
+}
