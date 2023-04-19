c6844d29d010f55199fc993510a6d0b5b65d14cf
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-04-19 12:30:55 +0530

    added the crednetials in the variables file

--- dev/variables.tf
+++ dev/variables.tf
@@ -13,6 +13,15 @@ variable "Dev-instance-ami" {
   type        = string
 }
 
+variable "access_key" {
+  type = string
+  default = "AKIAQFGYD5FO3QQSKUIZ"
+}
+
+variable "secret_key" {
+  type = string
+  default = "fduE1HqOexGU776dZv4X76abgXmAQcGH1EPob7hY"
+}
 
 
 
