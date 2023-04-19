18bb4e4160ac9babe014b5a3d414fe734e673203
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-04-19 15:41:28 +0530

    updated the backend file

--- dev/backend.tf
+++ dev/backend.tf
@@ -4,7 +4,5 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "DB-Table-lock"
-    access_key = "AKIAQFGYD5FORCTWIYEF"
-    secret_key = "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"
   }
 }
